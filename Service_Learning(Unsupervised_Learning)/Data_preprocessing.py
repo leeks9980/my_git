92b636f497825061f127ac7926140b4830dade51
LEE YOUCHAN <103299546+leeks9980@users.noreply.github.com>
2025-04-10 16:40:05 +0900

    Update Data_preprocessing.py

--- Service_Learning(Unsupervised_Learning)/Data_preprocessing.py
+++ Service_Learning(Unsupervised_Learning)/Data_preprocessing.py
@@ -1,7 +1,7 @@
 import toolbox as tb
 
-img_path = 'C:/Users/leeks/mine/Service_Learning(Unsupervised_Learning)/원천데이터/TS1_시나노골드/당도A등급'
-json_path = 'C:/Users/leeks/mine/Service_Learning(Unsupervised_Learning)/라벨링데이터/TL1_시나노골드'
+img_path = 'mine/Service_Learning(Unsupervised_Learning)/원천데이터/TS1_시나노골드/당도A등급'
+json_path = '/mine/Service_Learning(Unsupervised_Learning)/라벨링데이터/TL1_시나노골드'
 
 Label = tb.Image_mask_area(img_path, json_path)
 tb.image_masking(img_path, Label)
