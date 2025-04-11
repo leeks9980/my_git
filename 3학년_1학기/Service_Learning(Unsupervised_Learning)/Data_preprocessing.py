5daf75c18c8e40659aa5378e3c7c27b51bdcb285
LEE YOUCHAN <103299546+leeks9980@users.noreply.github.com>
2025-04-11 23:02:10 +0900

    Update Data_preprocessing.py

--- 3학년_1학기/Service_Learning(Unsupervised_Learning)/Data_preprocessing.py
+++ 3학년_1학기/Service_Learning(Unsupervised_Learning)/Data_preprocessing.py
@@ -1,7 +1,7 @@
 import toolbox as tb
 
-img_path = 'C:/Users/leeks/mine/Service_Learning(Unsupervised_Learning)/원천데이터/TS1_시나노골드/당도A등급'
-json_path = 'C:/Users/leeks/mine/Service_Learning(Unsupervised_Learning)/라벨링데이터/TL1_시나노골드'
+img_path = '/Service_Learning(Unsupervised_Learning)/원천데이터/TS1_시나노골드/당도A등급'
+json_path = '/Service_Learning(Unsupervised_Learning)/라벨링데이터/TL1_시나노골드'
 
 #이미지 이름 과 세그메테이션 이름 딕션너리
 Label = tb.Image_mask_area(img_path, json_path)
