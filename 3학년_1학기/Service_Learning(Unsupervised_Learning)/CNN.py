f3e584de9aefadd46f9c00cfbf3b93f4d444999b
LEE YOUCHAN <103299546+leeks9980@users.noreply.github.com>
2025-05-25 20:37:46 +0900

    Update CNN.py

--- 3학년_1학기/Service_Learning(Unsupervised_Learning)/CNN.py
+++ 3학년_1학기/Service_Learning(Unsupervised_Learning)/CNN.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam
 from sklearn.utils.class_weight import compute_class_weight
 import numpy as np
@@ -9,7 +9,7 @@ import numpy as np
 train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
 train_gen = train_datagen.flow_from_directory(
-    "파일 경로",
+    "학습 데이터 경로",
     target_size=(124, 124),
     batch_size=32,
     class_mode='binary',
@@ -17,7 +17,7 @@ train_gen = train_datagen.flow_from_directory(
 )
 
 val_gen = train_datagen.flow_from_directory(
-    "파일 경로",
+    "정답 데이터 경로",
     target_size=(124, 124),
     batch_size=32,
     class_mode='binary',
@@ -31,21 +31,34 @@ class_weight = dict(enumerate(class_weights))
 
 # 모델 구성
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(124, 124, 3)),
+    Conv2D(32, (3, 3), padding='same', input_shape=(124, 124, 3)),
+    BatchNormalization(),
     MaxPooling2D(2, 2),
-    Conv2D(64, (3, 3), activation='relu'),
+
+    Conv2D(64, (3, 3), padding='same'),
+    BatchNormalization(),
     MaxPooling2D(2, 2),
-    Flatten(),
+
+    Conv2D(128, (3, 3), padding='same'),
+    BatchNormalization(),
+    MaxPooling2D(2, 2),
+
+    Conv2D(256, (3, 3), padding='same'),
+    BatchNormalization(),
+    MaxPooling2D(2, 2),
+
+    GlobalAveragePooling2D(),
     Dense(64, activation='relu'),
     Dropout(0.3),
-    Dense(1, activation='sigmoid')  # 이진 분류
+    Dense(1, activation='sigmoid')
 ])
 
+
 # 컴파일
-model.compile(optimizer=Adam(learning_rate=0.00005), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
 
 # 학습
-model.fit(train_gen, validation_data=val_gen, epochs=100, class_weight=class_weight)
+model.fit(train_gen, validation_data=val_gen, epochs=500, class_weight=class_weight)
 
 #모델 저장
-#model.save('apple_classifier.h5')
+model.save('apple_classifier.h5')
