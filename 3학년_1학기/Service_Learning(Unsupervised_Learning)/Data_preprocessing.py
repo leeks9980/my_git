ea5e282c2555351291dca6b09f239ca6bb9d6aef
LEE YOUCHAN <103299546+leeks9980@users.noreply.github.com>
2025-05-25 23:25:00 +0900

    Update Data_preprocessing.py

--- 3학년_1학기/Service_Learning(Unsupervised_Learning)/Data_preprocessing.py
+++ 3학년_1학기/Service_Learning(Unsupervised_Learning)/Data_preprocessing.py
@@ -2,7 +2,6 @@ import toolbox as tb
 
 img_path = '이미지 파일 경로'
 json_path = 'json 파일 경로'
-save_path = "저장 경로"
 
 dirty_path = "비정상 이미지 경로"
 clean_path = "정상 이미지 경로"
@@ -13,5 +12,4 @@ preprocessor = tb.image_preprocessing(img_path, json_path)
 
 preprocessor.Image_mask_area()
 preprocessor.del_img()
-preprocessor.image_masking(save_path)
-preprocessor.cnn_classification(clean_path, dirty_path,model_path)
+preprocessor.mask_and_classify()
