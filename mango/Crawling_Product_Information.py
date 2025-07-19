fd3008e5f3954b5b54a1e7460e79f51ff9bb7e58
LEE YOUCHAN <103299546+leeks9980@users.noreply.github.com>
2025-07-20 00:56:45 +0900

    Update Crawling_Product_Information.py

--- mango/Crawling_Product_Information.py
+++ mango/Crawling_Product_Information.py
@@ -18,7 +18,7 @@ def fetch_full_html(url, output_path="full_page.html"):
         driver.get(url)
         
         pyautogui.moveTo(1101, 1354, duration=1)  #Physical coordinates
-        time.sleep(8)  #Time to go beyond security
+        time.sleep(10)  #Time to go beyond security
         pyautogui.scroll(-1145)
         time.sleep(4)  #Page loading time
         pyautogui.leftClick()
@@ -30,10 +30,8 @@ def fetch_full_html(url, output_path="full_page.html"):
     finally:
         driver.quit()
 
-
-
 #Find product information and product name
-def parse_product_info(html, df):
+def parse_product_info(html, Category):
     wanted_keys = [
         "사용기한(또는 개봉 후 사용기간)",
         "사용방법",
@@ -53,10 +51,11 @@ def parse_product_info(html, df):
             product_info[title] = content
     
     final_info = []
-
+    Product_Ingredients = []
     #Product name
     product_name = soup.find("p", class_="prd_name").text.strip()
     final_info.append(product_name)
+    Product_Ingredients.append(product_name)
 
     #Product price
     price_tag = soup.find("span", class_="price-2").find('strong')
@@ -69,20 +68,12 @@ def parse_product_info(html, df):
             if key == "화장품법에 따라 기재해야 하는 모든 성분":
                 # Split the ingredients by comma and remove any extra spaces
                 ingredients = [ingredient.strip() for ingredient in product_info[key].split(",")]
-                final_info.append(ingredients)
+                for i in ingredients:
+                    Product_Ingredients.append(i)
             else:
                 final_info.append(product_info[key])
         else:
             final_info.append("정보 없음")  # Default value if key not found
+    final_info.append(Category)
 
-    #Save only the parts you want
-    df.loc[len(df)] = final_info
-    print(df)
-    return product_info
-
-
-product_url = "https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000226486&dispCatNo=100000100010013&trackingCd=Cat100000100010013_Small&t_page=%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%EA%B4%80&t_click=%EC%8A%A4%ED%82%A8/%ED%86%A0%EB%84%88_%EC%A0%84%EC%B2%B4__%EC%83%81%ED%92%88%EC%83%81%EC%84%B8&t_number=38"
-html = fetch_full_html(product_url)
-
-df = pd.DataFrame(columns=['제품명', '가격', '사용기한', '사용방법', '성분'])
-info = parse_product_info(html, df)
+    return final_info, Product_Ingredients
